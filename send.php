69388babf761ca03e094d3babde4d54416a6150f
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2016-08-05 15:42:12 -0400

    uploading to server. Goin live

--- send.php
+++ send.php
@@ -1,39 +1,42 @@
 <?php if (!isset($_SESSION)) session_start();
 
-		if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
+	if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 
-		$name     	= $_POST['name'];
-        $email    	= $_POST['email'];
-        $message 	= $_POST['message'];
-        $error		= '';
+	$name     	= $_POST['name'];
+    $email    	= $_POST['email'];
+    $message 	= $_POST['message'];
+    $error		= '';
 
-		$address = "stefanbobrowski1@gmail.com";
+	$address = "stefanbobrowski1@gmail.com";
 
-		$e_subject = 'New message from ' . $name . ', ' . ', ' . $email  . '.';
+	$e_subject = 'New message from ' . $name . ', ' . ', ' . $email  . '.';
 
-		$e_body = "You have a new message from: $name, $email" . PHP_EOL . PHP_EOL;
-		$e_content = "Message: $message" . PHP_EOL . PHP_EOL;
-		$e_reply = "You can contact $name via email: $email";
+	$e_body = "You have a new message from: $name, $email" . PHP_EOL . PHP_EOL;
+	$e_content = "Message: $message" . PHP_EOL . PHP_EOL;
+	$e_reply = "You can contact $name via email: $email";
 
-		$msg = wordwrap($e_body . $e_content . $e_reply,70);
+	$msg = wordwrap($e_body . $e_content . $e_reply,70);
 
-		$headers = "From: $email" . PHP_EOL;
-		$headers .= "Reply-To: $email" . PHP_EOL;
-		$headers .= "MIME-Version: 1.0" . PHP_EOL;
-		$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-		$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
-		   
-		if(mail($address, $e_subject, $msg, $headers)) {
+	$headers = "From: $email" . PHP_EOL;
+	$headers .= "Reply-To: $email" . PHP_EOL;
+	$headers .= "MIME-Version: 1.0" . PHP_EOL;
+	$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+	$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+	   
+	if(mail($address, $e_subject, $msg, $headers)) {
 
-			echo "<div class='success-msg'>
-				<span class='icon-info-2'></span>
-				Hello, $name.<br>
-				Your message has been sent successfully.<br>
-				We will contact you soon!
-			</div>";
+		echo "<div class='success-msg'>
+			<span class='icon-info-2'></span>
+			Hello, $name.<br>
+			Your message has been sent successfully!<br>
+			I'll get back to you shortly.
+			<a href='/'>Back</a>
+		</div>";
 
-		} else {
+	} else {
 
-			echo 'ERROR! Your message could not be sent.';
+		echo 'ERROR! Your message could not be sent.';
 
-		}
+	}
+
+?>
