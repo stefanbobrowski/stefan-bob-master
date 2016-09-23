93c0f3770ac03a947c7b6b82bc7432421aaf234d
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2016-09-22 23:45:47 -0400

    change gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,28 +6,35 @@ var autoprefixer = require('gulp-autoprefixer');
 var browserSync  = require('browser-sync').create();
 
 gulp.task('styles', function() {
-  gulp.src('scss/**/*.scss')
-      .pipe(concat('main.css'))
-      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-      .pipe(autoprefixer({
-          browsers: ['last 4 versions'],
-          cascade: false
-      }))
-      .pipe(gulp.dest('css'));
+  return gulp.src('scss/**/*.scss') // Gets all files ending with .scss
+    .pipe(concat('main.css'))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 gulp.task('scripts', function() {
-   gulp.src(['scripts/jquery-3.0.0.min.js',
-       'scripts/jquery.matchHeight.js',
-       'scripts/main.js'])
-       .pipe(concat('main.js'))
-       .pipe(uglify())
-       .pipe(gulp.dest('js'));
-});
+  gulp.src('scripts/**/*.js')
+      .pipe(concat('main.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('js'));
+})
 
-//Watch task
-gulp.task('default', ['browserSync'], function() {
-    gulp.watch('scss/**/*.scss',['styles'], browserSync.reload);
-    gulp.watch('scripts/**/*.js',['scripts'], browserSync.reload);
-    gulp.watch('*.html', browserSync.reload);
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+    },
+  })
+})
+
+gulp.task('default', ['browserSync'], function (){
+  gulp.watch('scss/**/*.scss',['styles'], browserSync.reload);
+  gulp.watch('scripts/**/*.js',['scripts'], browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
 });
